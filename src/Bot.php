2df680436564272be008cf298f79c5f3047c95c5
uzsoftic <softuzb@gmail.com>
2023-02-07 13:49:02 +0500

    first commit

---
+++ src/Bot.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Uzsoftic\TelegramBot;
+
+use Illuminate\Support\Facades\Http;
+
+class Bot {
+
+    public function __construct(){
+
+    }
+
+    public function index(){
+
+    }
+
+}
---
+++ src/Providers/TelegramProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Uzsoftic\TelegramBot\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class TelegramProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+}
